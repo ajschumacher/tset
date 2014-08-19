22a7ccd57fd72a3b3102d1602b3d38b2ea7bb74b
Aaron Schumacher <ajschumacher@gmail.com>
2014-08-19 20:48:35 +0000

    more tests for changes over time

--- test.py
+++ test.py
@@ -37,6 +37,11 @@ class TestEdges(unittest.TestCase):
         self.assertEqual(t.get(before), set())
         self.assertEqual(t.get(before, just_value=False)[1], datetime.min)
 
+    def test_time_must_be_time(self):
+        t = tset()
+        with self.assertRaises(TypeError):
+            t.set(set(), 'now')
+
 class TestReturns(unittest.TestCase):
 
     def test_returns_set(self):
@@ -50,12 +55,7 @@ class TestReturns(unittest.TestCase):
         self.assertIs(type(response[0]), set)
         self.assertIs(type(response[1]), datetime)
 
-class TestSetting(unittest.TestCase):
-
-    def test_time_must_be_time(self):
-        t = tset()
-        with self.assertRaises(TypeError):
-            t.set(set(), 'now')
+class TestUpdates(unittest.TestCase):
 
     def test_single_growing_update(self):
         t = tset([1])
@@ -84,6 +84,43 @@ class TestSetting(unittest.TestCase):
         t.set([1, 2, 3], datetime.now() + week)
         self.assertEqual(t.get(), set())
         self.assertEqual(t.get(datetime.now() + 2*week), set([1, 2, 3]))
+
+    def test_past_update(self):
+        one = datetime.now()
+        two = datetime.now()
+        three = datetime.now()
+        t = tset([1, 2, 3])
+        t.set([3, 4], two)
+        # didn't change the present
+        self.assertEqual(t.get(), set([1, 2, 3]))
+        # didn't change beginning of time
+        self.assertEqual(t.get(one), set())
+        # change in place at time
+        self.assertEqual(t.get(two), set([3, 4]))
+        # and chronologically
+        self.assertEqual(t.get(three), set([3, 4]))
+
+    def test_intermediate_change_works_correctly(self):
+        one = datetime.now()
+        t = tset([1, 2, 3])
+        two = datetime.now()
+        three = datetime.now()
+        four = datetime.now()
+        t.set([3, 4, 5])
+        five = datetime.now()
+        t.set([2, 3, 4], three)
+        # final value still in place at time five
+        self.assertEqual(t.get(five), set([3, 4, 5]))
+        # and now
+        self.assertEqual(t.get(), set([3, 4, 5]))
+        # land before time still fine
+        self.assertEqual(t.get(one), set())
+        # initial value not messed with
+        self.assertEqual(t.get(two), set([1, 2, 3]))
+        # value we put in is there
+        self.assertEqual(t.get(three), set([2, 3, 4]))
+        # value we put in is kept there until later change
+        self.assertEqual(t.get(four), set([2, 3, 4]))
 
 
 if __name__ == '__main__':
