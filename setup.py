39837990f68c3eb9173eb7e9dd6c3b14e4fc1adc
Aaron Schumacher <ajschumacher@gmail.com>
2014-10-16 15:00:19 +0000

    PEP 8 changes per flycheck

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 with open('README.rst') as file:
     long_description = file.read()
 
-setup(name = 'tset',
-      description = 'time sets for Python',
-      long_description = long_description,
-      license = 'MIT',
-      author = 'Aaron Schumacher',
-      author_email = 'ajschumacher@gmail.com',
-      url = 'https://github.com/ajschumacher/tset',
-      download_url = 'https://github.com/ajschumacher/tset/tarball/0.1',
-      version = '0.1')
+setup(name='tset',
+      description='time sets for Python',
+      long_description=long_description,
+      license='MIT',
+      author='Aaron Schumacher',
+      author_email='ajschumacher@gmail.com',
+      url='https://github.com/ajschumacher/tset',
+      download_url='https://github.com/ajschumacher/tset/tarball/0.1',
+      version='0.1')
